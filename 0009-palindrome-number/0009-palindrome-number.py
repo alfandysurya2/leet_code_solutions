3252d21e39de0cb6f541bf892209a9edd464d51b
Alfandy Surya <88311779+alfandysurya2@users.noreply.github.com>
2024-02-01 12:49:20 +0700

    Time: 55 ms (65.49%), Space: 16.4 MB (83.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,12 +4,9 @@ class Solution:
         :type x: int
         :rtype: bool
         """
-        if x<0:
-            return False
+        x_str = str(x)
+        x_str_list = [i for i in x_str]
+        if x_str_list[::-1] == x_str_list:
+            return True
         else:
-            x_str = str(x)
-            x_str_list = [i for i in x_str]
-            if x_str_list[::-1] == x_str_list:
-                return True
-            else:
-                return False
+            return False
